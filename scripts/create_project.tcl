6e6cf324c5deb0e393b28e4839c4448fc637810e
Colm Ryan <cryan@bbn.com>
2016-03-21 15:17:43 -0400

    Update project creation script to use arguments

--- scripts/create_project.tcl
+++ scripts/create_project.tcl
@@ -1,24 +1,32 @@
 ###########################################################
-# Tcl script to create the PolyphaseSSB Vivado project
+# Tcl script to create the VHDL-Components Vivado project
 #
-# Usage: at the Tcl console set the PROJECT_DIR and PROJECT_NAME and
+# Usage: at the Tcl console manually set the argv to set the PROJECT_DIR and PROJECT_NAME and
 # then source this file. E.g.
 #
-# set PROJECT_DIR "/home/cryan/Programming/FPGA" or set PROJECT_DIR "C:/Users/qlab/Documents/Xilinx Projects/"
-# set PROJECT_NAME "PolyphaseSSB"
+# set argv [list "/home/cryan/Programming/FPGA" "VHDL-Components"] or
+# or  set argv [list "C:/Users/qlab/Documents/Xilinx Projects/" "VHDL-Components"]
 # source create_project.tcl
-
+#
+# from Vivado batch mode use the -tclargs to pass argv
+# vivado -mode batch -source create_project.tcl -tclargs "/home/cryan/Programming/FPGA" "VHDL-Components"
 ############################################################
 
-set REPO_PATH [file dirname [file normalize [info script]]]
+set PROJECT_DIR [lindex $argv 0]
+set PROJECT_NAME [lindex $argv 1]
+
+#Figure out the script path
+set SCRIPT_PATH [file normalize [info script]]
+set REPO_PATH [file dirname $SCRIPT_PATH]/../
 
 create_project -force $PROJECT_NAME $PROJECT_DIR/$PROJECT_NAME -part xc7a200tfbg676-2
-set_property target_language VHDL [current_project]
+set_property "default_lib" "xil_defaultlib" [current_project]
+set_property "sim.ip.auto_export_scripts" "1" [current_project]
+set_property "simulator_language" "Mixed" [current_project]
+set_property "target_language" "VHDL" [current_project]
 
 # add VHDL sources
 add_files -norecurse $REPO_PATH/ $REPO_PATH/src
-add_files $REPO_PATH/deps/VHDL-Components/src/ComplexMultiplier.vhd
-add_files $REPO_PATH/deps/VHDL-Components/src/DelayLine.vhd
 
 #testbenches
 add_files -norecurse -fileset sim_1 $REPO_PATH/test
@@ -30,10 +38,3 @@ set_property target_constrs_file $REPO_PATH/constraints/timing.xdc [current_file
 # ip cores
 set ip_srcs [glob $REPO_PATH/ip/*.xci]
 import_ip $ip_srcs
-
-set_property top Polyphase_SSB [current_fileset]
-update_compile_order -fileset sources_1
-
-set_property top Polyphase_SSB_tb [get_filesets sim_1]
-set_property top_lib xil_defaultlib [get_filesets sim_1]
-update_compile_order -fileset sim_1
