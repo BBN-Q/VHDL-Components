fa74351d65ad49faf045122e9912c88bcf008055
Colm Ryan <cryan@bbn.com>
2016-03-22 15:53:39 -0400

    :white_check_mark: improve test coverage and cleanup
    * cover imaginary component
    * make sure checking finishes
    * various tidy ups

--- test/PolyphaseSSB_tb.vhd
+++ test/PolyphaseSSB_tb.vhd
@@ -29,17 +29,15 @@ signal wfm_out_re  : std_logic_vector(4*OUT_DATA_WIDTH-1 downto 0);
 signal wfm_out_im  : std_logic_vector(4*OUT_DATA_WIDTH-1 downto 0);
 signal wfm_out_vld : std_logic := '0';
 
-signal wfm_check_re, wfm_check_im : std_logic_vector(15 downto 0) := (others => '0');
-
 signal wfm_oserdes_re, wfm_oserdes_im : std_logic_vector(15 downto 0);
 
-signal temp_realA, temp_realB, tempf_realA, tempf_realB : real;
 
 --define different stages for testbench, corresponding to different SSB parameters
 type TESTBENCH_STATE_t	is (RESETTING, BASEBAND_RAMP_RE, BASEBAND_PH_SHIFT, SSB_ON, SSB_PH_SHIFT, DONE);
 signal testbench_state : TESTBENCH_STATE_t;
 
 signal stop_the_clocks : boolean := false;
+signal checking_finished : boolean := false;
 
 begin
 
@@ -116,83 +114,111 @@ begin
 								& std_logic_vector(to_signed(8191, IN_DATA_WIDTH))
 								& std_logic_vector(to_signed(8191, IN_DATA_WIDTH))
 								& std_logic_vector(to_signed(8191, IN_DATA_WIDTH));
-	--shift phase by pi/2 (2^20, 1/4 circle)
-	phoff <= std_logic_vector(to_unsigned(4194304, 24));
-	wait for 10000ns;
+	--shift phase by pi/2 (2^22, 1/4 circle)
+	phoff <= std_logic_vector(to_unsigned(2**22, 24));
+	wait for 1000ns;
 
 	testbench_state <= SSB_ON;
 	phoff <= (others => '0');
-	--turn on SSB mod., 10 MHZ frequency (2^24, 1/10 clk)
-	--the default width for DDS phases (16 bit) does not give enough accuracy (phase error accumulates)
-	phinc <= std_logic_vector(to_unsigned(1677722, 24));
+	--turn on SSB mod.,  (2^24, 1/100 clk)
+	phinc <= std_logic_vector(to_unsigned((2**24)/64, 24));
 	wait for 10000ns;
 
 	testbench_state <= SSB_PH_SHIFT;
 	--shift phase by pi/2
-	phoff <= std_logic_vector(to_unsigned(4194304, 24));
+	phoff <= std_logic_vector(to_unsigned(2**22, 24));
 	wait for 10000ns;
 
 	testbench_state <= DONE;
+	wait for 100ns;
+	assert checking_finished report "Checking process failed to finish!";
 	stop_the_clocks <= true;
 	wait;
 end process;
 
 
 check : process
--- generate a (complex) signal to check wfm_out_xx against
-variable ind : integer range 0 to 1100 := 0;
+	variable ind : integer := 0;
+	variable wfm_check_re : signed(OUT_DATA_WIDTH-1 downto 0);
+	variable wfm_check_im : signed(OUT_DATA_WIDTH-1 downto 0);
+	variable slice_re : signed(OUT_DATA_WIDTH-1 downto 0);
+	variable slice_im : signed(OUT_DATA_WIDTH-1 downto 0);
 begin
 	--the while loop repeat until the condition is met for the first time (to sync. with wfm_out_xx).
 	wait until testbench_state = BASEBAND_RAMP_RE;
-	--wait for multiplier and oserdes delay
-	for ct in 0 to 9 loop
-		wait until rising_edge(clk_oserdes);
-	end loop ;
-	for i in -8192 to 8191 loop
+	wait until rising_edge(clk) and wfm_out_re /= std_logic_vector(to_signed(0, wfm_out_re'length));
+	for ct in -2048 to 2047 loop
 		--ramp amplitude
-		wfm_check_re <= std_logic_vector(to_signed(4*i, OUT_DATA_WIDTH));
-		--Arbitrarly allow 2 differences due to fixed point errors
-		assert abs(signed(wfm_check_re) - signed(wfm_oserdes_re)) <= 2 report "SSB output wrong in BASEBAND_RAMP_RE!";
-		wait until clk_oserdes'event; --only ok in simulation
+		for ct2 in 0 to 3 loop
+			wfm_check_re := to_signed(16*ct + 4*ct2, OUT_DATA_WIDTH);
+			--Arbitrarly allow difference of 2 due to fixed point errors
+			slice_re := signed(wfm_out_re((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			slice_im := signed(wfm_out_im((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			assert abs(wfm_check_re - slice_re) <= 2 report "real output wrong in BASEBAND_RAMP_RE! expected " & integer'image(to_integer(wfm_check_re)) & " but got " & integer'image(to_integer(slice_re)) ;
+			assert abs(slice_im) <= 2 report "imag output wrong in BASEBAND_RAMP_RE! expected " & integer'image(to_integer(wfm_check_im)) & " but got " & integer'image(to_integer(slice_im)) ;
+		end loop;
+		wait until rising_edge(clk);
 	end loop ;
+	report "Finished checking BASEBAND_RAMP_RE";
 
-	--shift phase by pi/2
-	wfm_check_im <= std_logic_vector(to_signed(32752, OUT_DATA_WIDTH));
-	wfm_check_re <= (others => '0');
-
-	wait until testbench_state = SSB_ON;
+	wait until rising_edge(clk) and testbench_state = BASEBAND_PH_SHIFT;
 	--wait until DDS is valid (10 cycles) and multiplier pipeline delay (4 cycles)
-	for ct in 0 to 29 loop
-		wait until rising_edge(clk_oserdes);
+	for ct in 0 to 14 loop
+		wait until rising_edge(clk);
+	end loop;
+	--shift phase by pi/2
+	wfm_check_im := to_signed(32762, OUT_DATA_WIDTH);
+	wfm_check_re := (others => '0');
+	assert abs(wfm_check_re - signed(wfm_oserdes_re)) <= 2 report "real output wrong in BASEBAND_PH_SHIFT!";
+	assert abs(wfm_check_im - signed(wfm_oserdes_im)) <= 2 report "imag output wrong in BASEBAND_PH_SHIFT!";
+	report "Finished checking BASEBAND_PH_SHIFT";
+
+	wait until rising_edge(clk) and testbench_state = SSB_ON;
+	--wait until DDS is valid (10 cycles) and multiplier pipeline delay (4 cycles)
+	for ct in 0 to 14 loop
+		wait until rising_edge(clk);
 	end loop ;
 
+	ind := 7;
 	while testbench_state /= SSB_PH_SHIFT loop
-		ind := ind+1;
-		tempf_realA  <= cos(MATH_PI*real(ind)/20.0);
-		tempf_realB  <= sin(MATH_PI*real(ind)/20.0);
-		wfm_check_re <= std_logic_vector(to_signed(integer(tempf_realA*real(32752)), OUT_DATA_WIDTH));
-		wfm_check_im <= std_logic_vector(to_signed(integer(tempf_realB*real(32752)), OUT_DATA_WIDTH));
-		--Arbitrarly allow 15 differences due to fixed point errors
-		assert abs(signed(wfm_check_re) - signed(wfm_oserdes_re)) <= 15 report "SSB output wrong in SSB_ON!";
-		wait until clk_oserdes'event;
+		for ct2 in 0 to 3 loop
+			ind := ind+1;
+			wfm_check_re := to_signed(integer(cos(2.0*MATH_PI*0.25*(1.0/64)*real(ind))*32762.0), OUT_DATA_WIDTH);
+			wfm_check_im := to_signed(integer(sin(2.0*MATH_PI*0.25*(1.0/64)*real(ind))*32762.0), OUT_DATA_WIDTH);
+			--Arbitrarly allow differences of 8 due to fixed point errors
+			slice_re := signed(wfm_out_re((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			slice_im := signed(wfm_out_im((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			assert abs(wfm_check_re - slice_re) <= 8 report "real output wrong in SSB_ON! expected " & integer'image(to_integer(wfm_check_re)) & " but got " & integer'image(to_integer(slice_re)) ;
+			assert abs(wfm_check_im - slice_im) <= 8 report "imag output wrong in SSB_ON! expected " & integer'image(to_integer(wfm_check_im)) & " but got " & integer'image(to_integer(slice_im)) ;
+		end loop;
+		wait until rising_edge(clk);
+	end loop;
+	report "Finished checking SSB_ON";
+
+	--wait until DDS is valid (10 cycles) and multiplier pipeline delay (4 cycles)
+	for ct in 0 to 14 loop
+		wait until rising_edge(clk);
+		ind := ind + 4;
 	end loop;
 
-	ind := 0;
-	--wait until DDS is valid (9 cycles) and multiplier pipeline delay (4 cycles)
-	for ct in 0 to 28 loop
-		wait until rising_edge(clk_oserdes);
-	end loop ;
-
-	while not stop_the_clocks loop
-		ind := ind+1;
-		tempf_realA  <= cos(MATH_PI*real(ind)/20.0 + MATH_PI/2.0);
-		tempf_realB  <= sin(MATH_PI*real(ind)/20.0 + MATH_PI/2.0);
-		wfm_check_re <= std_logic_vector(to_signed(integer(tempf_realA*real(32752)), OUT_DATA_WIDTH));
-		wfm_check_im <= std_logic_vector(to_signed(integer(tempf_realB*real(32752)), OUT_DATA_WIDTH));
-		assert abs(signed(wfm_check_re) - signed(wfm_oserdes_re)) <= 15 report "SSB output wrong in SSB_PH_SHIFT";
-		wait until clk_oserdes'event;
+	while testbench_state /= DONE loop
+		for ct2 in 0 to 3 loop
+			ind := ind+1;
+			--cos(2pi * freq * time); freq and time are in timestep units; freq is 1/4 because of 4 wide samples
+			wfm_check_re := to_signed(integer(cos(2.0*MATH_PI*0.25*(1.0/64)*real(ind) + MATH_PI/2.0)*32762.0), OUT_DATA_WIDTH);
+			wfm_check_im := to_signed(integer(sin(2.0*MATH_PI*0.25*(1.0/64)*real(ind) + MATH_PI/2.0)*32762.0), OUT_DATA_WIDTH);
+			--Arbitrarly allow difference of 8 due to fixed point errors
+			slice_re := signed(wfm_out_re((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			slice_im := signed(wfm_out_im((ct2+1)*OUT_DATA_WIDTH-1 downto ct2*OUT_DATA_WIDTH));
+			assert abs(wfm_check_re - slice_re) <= 8 report "real output wrong in SSB_PH_SHIFT! expected " & integer'image(to_integer(wfm_check_re)) & " but got " & integer'image(to_integer(slice_re)) ;
+			assert abs(wfm_check_im - slice_im) <= 8 report "imag output wrong in SSB_PH_SHIFT! expected " & integer'image(to_integer(wfm_check_im)) & " but got " & integer'image(to_integer(slice_im)) ;
+		end loop;
+		wait until rising_edge(clk);
 	end loop;
+	report "Finished checking SSB_PH_SHIFT";
 
+	checking_finished <= true;
+	wait;
 end process ;
 
 end;
