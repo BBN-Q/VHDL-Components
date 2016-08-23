bd936873524df40f3ebb0e3d69b84c4cf6e14afe
Colm Ryan <colm@colmryan.org>
2016-08-23 10:51:40 -0400

    constraints for synchronizers in generate loop
    have to escape [ ] brackets

--- constraints/synchronizer.tcl
+++ constraints/synchronizer.tcl
@@ -3,7 +3,10 @@ set synchronizers [get_cells -hier -filter {(ORIG_REF_NAME == synchronizer) || (
 
 foreach sync $synchronizers {
 	# false path to asynchronous preset or clear on all flip-flops in chain
-	set_false_path -to [get_pins -regexp "$sync/sync_chain.*/(PRE|CLR)"]
+	# synchronizer in generate loop might have [idx] brackets to specify loop index
+	# which need to be escaped for regexp
+	set pattern [string map {[ \\[ ] \\]} $sync/sync_chain_reg\[\\d\]/(PRE|CLR)]
+	set_false_path -to [get_pins -regexp $pattern]
 	# false path to first flip-flop data pin
 	set_false_path -to [get_pins $sync/sync_chain_reg[0]/D]
 	# max delay to maximize metastability settle time (assumes two flip-flops)
