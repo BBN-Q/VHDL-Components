b1538ba4e29dd5e45f69ce335824e63761a9191b
Colm Ryan <colm@colmryan.org>
2016-09-01 20:21:11 -0400

    handle synchronizers with no reset which will have no PRE/CLR pin

--- constraints/synchronizer.tcl
+++ constraints/synchronizer.tcl
@@ -5,8 +5,13 @@ foreach sync $synchronizers {
 	# false path to asynchronous preset or clear on all flip-flops in chain
 	# synchronizer in generate loop might have [idx] brackets to specify loop index
 	# which need to be escaped for regexp
-	set pattern [string map {[ \\[ ] \\]} $sync/sync_chain_reg\[\\d\]/(PRE|CLR)]
-	set_false_path -to [get_pins -regexp $pattern]
+	set reset_pins_pattern [string map {[ \\[ ] \\]} $sync/sync_chain_reg\[\\d\]/(PRE|CLR)]
+	set reset_pins [get_pins -regexp $reset_pins_pattern]
+	# if reset is a constant then there will be no PRE/CLR pins
+	if { [llength $reset_pins] > 0 } {
+		set_false_path -to $reset_pins
+	}
+
 	# false path to first flip-flop data pin
 	set_false_path -to [get_pins $sync/sync_chain_reg[0]/D]
 	# max delay to maximize metastability settle time (assumes two flip-flops)
