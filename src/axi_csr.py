ccbdf92d33cfe71d5a829caa58aecd398ad14e3d
Colm Ryan <colm@colmryan.org>
2016-10-05 09:46:25 -0400

    * allow variable register widths * infer number of registers and address width needed * actually use read signals in read process

--- src/axi_csr.py
+++ src/axi_csr.py
@@ -1,6 +1,8 @@
 """
 Python script to create an AXI memory mapped CSR.
 """
+
+from math import ceil, log2
 from jinja2 import Template
 
 class Register(object):
@@ -11,9 +13,8 @@ class Register(object):
 		self.mode = mode
 		self.initial_value = initial_value
 
-t = Template("""
--- AXI memory mapped CSR registers
-
+t = Template("""-- AXI memory mapped CSR registers
+--
 -- Original authors: Colm Ryan, Brian Donovan
 -- Copyright 2015-2016, Raytheon BBN Technologies
 
@@ -26,28 +27,30 @@ entity {{module_name}} is
 port (
 	-- CSR ports
 {%- for reg in register_map.values() %}
-	{{reg.label.ljust(just_width)}} : {{'in' if reg.mode=='read' else 'out'}} std_logic_vector(31 downto 0);
+	{%- if reg.mode == 'write' or reg.mode == 'read' %}
+	{{reg.label.ljust(PORT_JUSTIFICATION_WIDTH)}} : {{'in' if reg.mode=='read' else 'out'}} std_logic_vector(31 downto 0);
+	{%- endif %}
 {%- endfor %}
 
 	-- slave AXI bus
 	s_axi_aclk    : in std_logic;
 	s_axi_aresetn : in std_logic;
-	s_axi_awaddr  : in std_logic_vector({{S_AXI_ADDR_WIDTH}}-1 downto 0);
+	s_axi_awaddr  : in std_logic_vector({{AXI_ADDR_WIDTH-1}} downto 0);
 	s_axi_awprot  : in std_logic_vector(2 downto 0);
 	s_axi_awvalid : in std_logic;
 	s_axi_awready : out std_logic;
-	s_axi_wdata   : in std_logic_vector({{S_AXI_DATA_WIDTH}}-1 downto 0);
-	s_axi_wstrb   : in std_logic_vector(({{S_AXI_DATA_WIDTH}}/8)-1 downto 0);
+	s_axi_wdata   : in std_logic_vector({{REGISTER_WIDTH-1}} downto 0);
+	s_axi_wstrb   : in std_logic_vector({{REGISTER_BYTE_WIDTH-1}} downto 0);
 	s_axi_wvalid  : in std_logic;
 	s_axi_wready  : out std_logic;
 	s_axi_bresp   : out std_logic_vector(1 downto 0);
 	s_axi_bvalid  : out std_logic;
 	s_axi_bready  : in std_logic;
-	s_axi_araddr  : in std_logic_vector({{S_AXI_ADDR_WIDTH}}-1 downto 0);
+	s_axi_araddr  : in std_logic_vector({{AXI_ADDR_WIDTH-1}} downto 0);
 	s_axi_arprot  : in std_logic_vector(2 downto 0);
 	s_axi_arvalid : in std_logic;
 	s_axi_arready : out std_logic;
-	s_axi_rdata   : out std_logic_vector({{S_AXI_DATA_WIDTH}}-1 downto 0);
+	s_axi_rdata   : out std_logic_vector({{REGISTER_WIDTH-1}} downto 0);
 	s_axi_rresp   : out std_logic_vector(1 downto 0);
 	s_axi_rvalid  : out std_logic;
 	s_axi_rready  : in std_logic
@@ -57,9 +60,9 @@ end entity;
 architecture arch of {{module_name}} is
 
 	-- register size (AXI address is byte wide)
-	constant NUM_REGS : natural := 2**({{S_AXI_ADDR_WIDTH}}-2);
+	constant NUM_REGS : natural := {{NUM_REGS}};
 	-- array of registers
-	type REG_ARRAY_t is array(natural range <>) of std_logic_vector({{S_AXI_DATA_WIDTH}}-1 downto 0) ;
+	type REG_ARRAY_t is array(natural range <>) of std_logic_vector({{REGISTER_WIDTH-1}} downto 0) ;
 	signal regs : REG_ARRAY_t(0 to NUM_REGS-1) := (others => (others => '0'));
 	signal write_reg_addr : integer range 0 to NUM_REGS-1;
 	signal read_reg_addr  : integer range 0 to NUM_REGS-1;
@@ -67,8 +70,8 @@ architecture arch of {{module_name}} is
 	-- internal AXI signals
 	signal axi_awready : std_logic;
 	signal axi_wready  : std_logic;
-	signal axi_wdata   : std_logic_vector({{S_AXI_DATA_WIDTH}}-1 downto 0);
-	signal axi_wstrb   : std_logic_vector(({{S_AXI_DATA_WIDTH}}/8)-1 downto 0);
+	signal axi_wdata   : std_logic_vector({{REGISTER_WIDTH-1}} downto 0);
+	signal axi_wstrb   : std_logic_vector({{REGISTER_BYTE_WIDTH-1}} downto 0);
 	signal axi_bvalid  : std_logic;
 	signal axi_arready : std_logic;
 	signal axi_rvalid  : std_logic;
@@ -132,7 +135,7 @@ begin
 	begin
 		if rising_edge(s_axi_aclk) then
 			-- decode register address
-			write_reg_addr <= to_integer(unsigned(s_axi_awaddr(s_axi_awaddr'high downto 2)));
+			write_reg_addr <= to_integer(unsigned(s_axi_awaddr(s_axi_awaddr'high downto {{LOG2_REGISTER_BYTE_WIDTH}})));
 			-- register data and byte enables
 			axi_wdata <= s_axi_wdata;
 			axi_wstrb <= s_axi_wstrb;
@@ -147,7 +150,7 @@ begin
 
 			-- otherwise update the addressed register
 			else
-				for ct in 0 to ({{S_AXI_DATA_WIDTH}}/8-1) loop
+				for ct in 0 to {{REGISTER_BYTE_WIDTH-1}} loop
 					if axi_wready = '1' and axi_wstrb(ct) = '1' then
 						regs(write_reg_addr)(ct*8+7 downto ct*8) <= axi_wdata(ct*8+7 downto ct*8);
 					end if;
@@ -171,10 +174,10 @@ begin
 				axi_rvalid <= '0';
 			else
 				-- acknowledge and latch address when no outstanding read responses
-				if axi_arready = '0' and axi_rvalid = '1' then
+				if axi_arready = '0' and s_axi_arvalid = '1' then
 					axi_arready <= '1';
 					-- latch register address
-					read_reg_addr <= to_integer(unsigned(s_axi_awaddr(s_axi_awaddr'high downto 2)));
+					read_reg_addr <= to_integer(unsigned(s_axi_araddr(s_axi_araddr'high downto {{LOG2_REGISTER_BYTE_WIDTH}})));
 				else
 					axi_arready <= '0';
 				end if;
@@ -198,6 +201,36 @@ end architecture;
 """
 )
 
+def write_axi_csr(filename, register_map, register_width=32):
+
+	# check that register width is a power of 2
+	assert int(log2(register_width)) == log2(register_width), "register_width must be power of 2"
+	register_byte_width = register_width // 8
+	log2_register_byte_width = int(log2(register_byte_width))
+
+	# figure out some constants
+	ceil_log2_num_regs = ceil( log2(max(register_map.keys()) ) )
+	axi_addr_width = ceil_log2_num_regs + log2_register_byte_width
+	num_regs = 2**(ceil_log2_num_regs)
+
+	# maximum register width for some alignment nicieties
+	port_justification_width = max(len(reg.label) for reg in register_map.values())
+
+	with open("AXI_CSR.vhd", "w") as FID:
+		FID.write(
+			t.render(
+				module_name="AXI_CSR",
+				register_map=register_map,
+				PORT_JUSTIFICATION_WIDTH=port_justification_width,
+				NUM_REGS=num_regs,
+				AXI_ADDR_WIDTH=axi_addr_width,
+				REGISTER_WIDTH=register_width,
+				REGISTER_BYTE_WIDTH=register_byte_width,
+				LOG2_REGISTER_BYTE_WIDTH=log2_register_byte_width
+				)
+			)
+
+
 
 if __name__ == '__main__':
 	register_map = {
@@ -205,16 +238,7 @@ if __name__ == '__main__':
 					1 : Register("status2", "read"),
 					2 : Register("control1", "write", initial_value=0xffffffff),
 					3 : Register("control2", "write", initial_value=0x01234567),
+					5 : Register("scratch", "internal", initial_value=0x000dba11)
 				}
 
-
-	with open("AXI_CSR.vhd", "w") as FID:
-		FID.write(
-			t.render(
-				module_name="AXI_CSR",
-				register_map=register_map,
-				just_width=10,
-				S_AXI_ADDR_WIDTH=8,
-				S_AXI_DATA_WIDTH=32
-				)
-			)
+	write_axi_csr("AXI_CSR.vhd", register_map, 32)
